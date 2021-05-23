483b8f2f9d668d0681ac15c9449cf6a4f21b4ec9
Thomas Poupinet <th.poupinet@free.fr>
2021-05-23 13:34:33 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function makeRequest() {
+    fetch(​"http://localhost:3000/api/furniture")
+    .then(function(res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function(value) {
+        console.log(value);
+    })
+    .catch(function(err) {
+        // Une erreur est survenue
+    });
+}
+makeRequest()
