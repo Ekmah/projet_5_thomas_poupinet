bccc0a244d2ec3086f3db3defc1033502aef0998
Thomas Poupinet <th.poupinet@free.fr>
2021-05-23 14:06:47 +0200

    added bootstrap and fetch loop

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,19 @@ function makeRequest() {
     })
     .then(function(value) {
         console.log(value);
+        for (thing of value) {
+            console.log(thing);
+            let content = document.getElementById("mainContent")
+            content.innerHTML +="\
+            <div class='card' style='width: 18rem;'>\
+                <img src='"+thing.imageUrl+"' class='card-img-top' alt='...'> \
+                <div class='card-body'> \
+                    <h5 class='card-title'>"+thing.name+"</h5>\
+                    <p class='card-text'>"+thing.description+"</p>\
+                    <a href='#' class='btn btn-primary'>Go somewhere</a>\
+                </div>\
+            </div>"
+        }
     })
     .catch(function(err) {
         // Une erreur est survenue
